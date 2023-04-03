d57a4f6558662f8aafcb0310a9f34652ef597634
StarGrim33 <prosky33@gmail.com>
2023-04-03 16:53:26 +0300

    Добавил паузу музыки при включении меню

--- My project (1)/Assets/Scripts/UI/Menu.cs
+++ My project (1)/Assets/Scripts/UI/Menu.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Menu : MonoBehaviour
 {
+    [SerializeField] private AudioSource _musicAudioSource;
+
     public void OpenPanel(GameObject panel)
     {
         panel.SetActive(true);
         Time.timeScale = 0f;
+        _musicAudioSource.Pause();
     }
 
     public void ClosePanel(GameObject panel)
     {
         panel.SetActive(false);
         Time.timeScale = 1f;
+        _musicAudioSource.UnPause();
     }
 }
